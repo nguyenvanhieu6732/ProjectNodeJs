a1975359f91d53bf14a1e8b251c73a39da39396d
Xin chào Hiếu !!! <Nguyenhieu6732@gmail.com>
2024-04-14 23:41:40 +0700

    14/4/2024

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const CollectionsRouter = require('./collections')
 const blogRouter = require('./blog')
+const PagesRouter = require('./pages')
 const SiteRouter = require('./site')
 
 function route(app) {
     app.use('/collections', CollectionsRouter);
     // app.use('/pages', pagesRouter);
     app.use('/blog', blogRouter);
+    app.use('/pages', PagesRouter);
     app.use('/', SiteRouter);
 
 }
